6c3330ab0210f52aa385c7734043403d8aa89d69
Charles-WYC <727304783@qq.com>
2019-06-06 05:36:49 +0000

    modify mtu

--- build.sh
+++ build.sh
@@ -8,15 +8,15 @@ hosts=(km-1 km-2 km-3 kn-1 kn-2 kn-3)
 for host in ${hosts[@]}
 do
     # distribute resources to each host
-    scp -r ${kubeDir}/releases ${host}:/tmp/
-    scp ${kubeDir}/kube.tar ${host}:~/
+    # scp -r ${kubeDir}/releases ${host}:/tmp/
+    # scp ${kubeDir}/kube.tar ${host}:~/
 
     # chomd
     ssh ${host} "sudo cp /tmp/releases/kubeadm /usr/local/bin/kubeadm"
     ssh ${host} "sudo chmod +x /usr/local/bin/kubeadm"
 
     # load images
-    ssh ${host} "sudo docker load -i kube.tar"
+    # ssh ${host} "sudo docker load -i kube.tar"
 done
 
 # build cluster
